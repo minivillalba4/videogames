0a338febf1cd8c058a6860301f9dc7f214effedc
minivillalba4 <134150180+minivillalba4@users.noreply.github.com>
2024-09-18 10:22:40 +0200

    Add files via upload

---
+++ videojuego.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler
+from sklearn.metrics import *
+import pickle
+import streamlit as st
+from joblib import load
+
+#Importar modelos con pickle
+model= load("C:/Users/isma_/Desktop/VS Code/Videojuego/model_videogame (2).pkl")
+target_encoder=load("C:/Users/isma_/Desktop/VS Code/Videojuego/target_encoder_videogame (1).pkl")
+scaler_X= load("C:/Users/isma_/Desktop/VS Code/Videojuego/scaler_x (1).pkl")
+
+
+#Importar el df original
+df=pd.read_csv("C:/Users/isma_/Downloads/archive (14)/online_gaming_behavior_dataset.csv")
+
+#Preguntar al usuario
+edad=st.sidebar.slider("Edad del usuario",1.0,140.0,df["Age"].mean(),1.0)
+sexo = st.sidebar.radio("Sexo", ["Hombre", "Mujer"])
+sexo_dict = {"Hombre": [1, 0], "Mujer": [0, 1]}
+sexo_values = sexo_dict[sexo]
+sexo_df = pd.DataFrame([sexo_values], columns=["Gender_Male", "Gender_Female"])
+sexo_df.drop(columns=["Gender_Female"],axis=1,inplace=True)
+localizacion=st.sidebar.selectbox("Localizacion",['Other', 'USA', 'Europe', 'Asia'])
+genero_videojuego=st.sidebar.selectbox("Género del videojuego",['Estrategia', 'Deportes', 'Acción', 'RPG', 'Simulación'])
+horas_juego=st.sidebar.slider("Horas de Juego",1.0,df["PlayTimeHours"].max()+10.0,df["PlayTimeHours"].mean(),1.0)
+compras_en_videojuegos=st.sidebar.radio("Ha realizado compras en videojuegos",["No","Si"])
+dificultad_videojuego=st.sidebar._select_slider("Localizacion",['Facil', 'Normal', 'Difícil'])
+sesiones_semanales=st.sidebar.slider("Sesiones por semana",0.0,df["PlayTimeHours"].max()+7.0,df["PlayTimeHours"].mean(),1.0)
+duracion_sesion=st.sidebar.slider("Duracion de la sesion",1.0,df["AvgSessionDurationMinutes"].max()+7.0,df["AvgSessionDurationMinutes"].mean(),1.0)
+nivel_jugador=st.sidebar.slider("Nivel del jugador",1.0,df["PlayerLevel"].max()+7.0,df["PlayerLevel"].mean(),1.0)
+logros_desbloqueados=st.sidebar.slider("Logros desbloqueados",0.0,df["AchievementsUnlocked"].max()+7.0,df["AchievementsUnlocked"].mean(),1.0)
+
+
+features={
+    'Age':edad,
+    'Location':localizacion,
+    'GameGenre':genero_videojuego,
+    'PlayTimeHours':horas_juego,
+    'InGamePurchases':compras_en_videojuegos,
+    'GameDifficulty':dificultad_videojuego,
+    'SessionsPerWeek':sesiones_semanales,
+    'AvgSessionDurationMinutes':duracion_sesion,
+    'PlayerLevel':nivel_jugador,
+    'AchievementsUnlocked':logros_desbloqueados,
+}
+data=pd.DataFrame(features,index=[0])
+data = pd.concat([data, sexo_df], axis=1)
+
+st.title("Prediccion modelo videojuegos")
+if st.checkbox("Mostrar dimensión del conjunto de datos"):
+    st.text("Dimensión del conjunto de datos:")
+    st.text(df.shape)
+if st.checkbox("Mostrar conjunto de datos"):
+    st.dataframe(df.head())
+if st.checkbox("Mostrar estadísticos principales del conjunto de datos"):
+    st.dataframe(df.describe())
+#Transformación de los datos
+df["EngagementLevel"]=df["EngagementLevel"].map({"Low":0,"Medium":1,"Hard":2})
+data["GameDifficulty"]=data["GameDifficulty"].map({"Facil":0,"Normal":1,"Difícil":2})
+data["InGamePurchases"]=data["InGamePurchases"].map({"No":0,"Si":1})
+
+#Targer encoder
+data=target_encoder.transform(data)
+
+#Reordenar
+expected_order=scaler_X.feature_names_in_
+data=data[list(expected_order)]
+
+#Escalador
+data_sc=scaler_X.transform(data)
+
+st.subheader("Prediccion")
+if st.checkbox("Mostrar predicción"):
+    st.write("La predicción es")
+    st.dataframe(model.predict(data_sc))
+
